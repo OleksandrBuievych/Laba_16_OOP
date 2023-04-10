f2b46a55829335dafc7f7ca2a994fcac4dd67041
OleksandrBuievych <112573653+OleksandrBuievych@users.noreply.github.com>
2023-04-11 01:19:35 +0300

    Добавьте файлы проекта.

---
+++ Laba_16_OOP/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Laba_16_OOP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Vector A; // Створюємо два вектори, з якими ми будемо проводити операції.
+        Vector B;
+        private void button1_Click(object sender, EventArgs e) // Створення вектора за 3 параметрами
+        {
+            try {   // Оброблюємо виключення(пусті текстБокси)
+                if (A == null)
+                {
+                    A = new Vector(int.Parse(textBox1.Text), int.Parse(textBox2.Text), int.Parse(textBox3.Text)); // Зчитуємо дані про вектор, з текстБоксу
+                    MessageBox.Show(A.Show());
+                }
+                else if (B == null)
+                {
+                    B = new Vector(int.Parse(textBox1.Text), int.Parse(textBox2.Text), int.Parse(textBox3.Text));
+                    MessageBox.Show(B.Show());
+                }
+                else MessageBox.Show("Ви уже створили 2 вектора. Виконайте дії з ними!");
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Будь-ласка введіть коректні дані!");
+            }
+        }
+         
+
+        private void button2_Click(object sender, EventArgs e)  // Створення вектора за двома параметрами
+        {
+            try
+            {
+                if (A == null)
+                {
+                    A = new Vector(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+                    MessageBox.Show(A.Show());
+                }
+                else if (B == null)
+                {
+                    B = new Vector(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+                    MessageBox.Show(B.Show());
+                }
+                else MessageBox.Show("Ви уже створили 2 вектора. Виконайте дії з ними!");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Будь-ласка введіть коректні дані!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) // Створення вектора за одним параметром
+        {
+            try    
+            {
+                if (A == null)
+                {
+                    A = new Vector(int.Parse(textBox1.Text));
+                    MessageBox.Show(A.Show());
+                }
+                else if (B == null)
+                {
+                    B = new Vector(int.Parse(textBox1.Text));
+                    MessageBox.Show(B.Show());
+                }
+                else MessageBox.Show("Ви уже створили 2 вектора. Виконайте дії з ними!");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Будь-ласка введіть коректні дані!");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e) // Кнопка "Виконати обчислення"
+        {
+            if (A != null && B != null) // Перевіряємо чи створено у нас 2 вектори
+            {
+                Vector PlusAndMinus = A + B;    // Додатково створюємо вектор, для передачі в нього координат, які ми отримали після суми\різниці
+                label10.Text = PlusAndMinus.ShowCoordination();
+                PlusAndMinus = A - B;
+                label11.Text = PlusAndMinus.ShowCoordination();
+                label12.Text = Convert.ToString(A * B);
+                label13.Text = Convert.ToString(A.Length());
+                label14.Text = Convert.ToString(B.Length());
+                label15.Text = Convert.ToString(PlusAndMinus.CosAngle(A,B));
+                
+            }
+            else MessageBox.Show("Будь-ласка спочатку створіть два вектори!");
+        }
+    }
+}
---
+++ Laba_16_OOP/Vector.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_16_OOP
+{
+    class Vector
+    {
+        // Координати кінців вектору
+        private double x;
+        private double y;
+        private double z;
+
+        // Конструктор для ініціалізації координат
+        public Vector(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public Vector(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = 0;
+        }
+
+        public Vector(double x)
+        {
+            this.x = x;
+            this.y = 0;
+            this.z = 0;
+        }
+
+        // Операції додавання та віднімання векторів
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+        }
+
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+        }
+
+        // Обчислення скалярного добутку двох векторів
+        public static double operator *(Vector v1, Vector v2)
+        {
+            return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+        }
+
+        // Обчислення довжини вектора
+        public double Length()
+        {
+            return Math.Sqrt(x * x + y * y + z * z);
+        }
+
+        // Обчислення косинуса кута між векторами
+        public double CosAngle(Vector v1, Vector v2)
+        {
+            double dotProduct = v1 * v2;
+            double lengthProduct = v1.Length() * v2.Length();
+            return dotProduct / lengthProduct;
+        }
+
+        public string Show()
+        {
+            return $"Ви успішно створили вектор ({this.x},{this.y},{this.z})";
+        }
+        
+        public string ShowCoordination()
+        {
+            return $"({this.x},{this.y},{this.z})";
+        }
+    }
+}
